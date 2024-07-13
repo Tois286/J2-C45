86bb3624d844fd08003df3aeed0fa27abe806f6c
Tois286 <itoto1937@gmail.com>
2024-07-13 09:32:34 +0700

    text box

--- view/dataSiswa.php
+++ view/dataSiswa.php
@@ -5,9 +5,8 @@
             <form action="modul/database/uploadPros.php" method="post" enctype="multipart/form-data">
                 <input type="file" name="excelFile" accept=".xlsx, .xls">
                 <button type="submit" class="button button1">Upload</button>
-
             </form>
-            <button class="button button1" style="background-color:#009879; color:white; width:55%; font-size: 12px;">file Wjib Excel, Menggunakan Huruf kapital dan Tandapa spasi!! (IPS1,KETERANGAN,DLL)</button>
+            <button class="button button1" style="background-color:#009879; color:white; width:70%; font-size: 12px;">Menggunakan Huruf kapital dan Tanpa spasi!! (WAJIB memiliki kolom IPS dan KETERANGAN, dengan Format xlsx)</button>
         </div>
     </div>
 </div>
